60db2a395bd8f5c2309b04633b66d4edb78f1ebb
Ekle Williams <willy2kle@mail.com>
2019-10-07 12:42:45 +0100

    Remove extra folder

--- extra/library_management_system/Library Management System/admin/session.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();
-if (!isset($_SESSION['id'])){
-header('location:index.php');
-}
-$id_session=$_SESSION['id'];
-?>
--- extra/library_management_system/Library Management System/admin/update_members_status.php
+++
@@ -1,7 +0,0 @@
-	<?php include ('include/dbcon.php');
-	
-	mysql_query ("UPDATE user SET status = 'Active' ")or die(mysql_error());
-	
-	echo "<script> window.location='user.php' </script>";
-	
-	?>			
